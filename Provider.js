216a9affea0be998e097bf3ab8e633f9fa079f70
Riley Mathews <rileymathews80@gmail.com>
2018-07-04 13:49:25 -0500

    added provider component

---
+++ Provider.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react"
+
+/*
+    This new function in React - createContext() - is what will
+    allow you to create a central store of data, and have any
+    component become a Consumer of the data that it cares about.
+*/
+export const Context = React.createContext()
+
+export class Provider extends Component {
+
+
+    /*
+    The initial state of the data provider should include
+    default values for any top-level component that will
+    need the data. In this case, PoliticianList is my only
+    top-level component. It is not a child of any other
+    component.
+    */
+    state = {
+        
+    }
+
+    
+
+    /*
+        Since this is just an ordinary component that extends
+        React.Component, you can use `componentDidMount` to
+        hit your API and then update state.
+    */
+
+
+    /*
+        This component will not render any DOM element itself.
+        Rather it becomes a virtual wrapper around any component
+        that wants to serve as the data provider for its children.
+    */
+    render() {
+        return (
+            <Context.Provider value={{
+                state: this.state,
+                
+            }}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
