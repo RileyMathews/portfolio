005f6a4d0ba02c3dc525a2724e8d0a46350394ab
Riley Mathews <rileymathews80@gmail.com>
2018-07-04 13:42:51 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './nav/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h1>Riley Mathews</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/nav/Nav.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {Navbar, NavbarBrand, NavbarItem, NavbarBurger, NavbarMenu, NavbarEnd} from 'bloomer'
+
+class Nav extends Component {
+    state = {
+        isActive: false
+    }
+
+    onClickNav = (evt) => {
+        this.setState({isActive: this.state.isActive ? false : true})
+    }
+
+    render() {
+        return (
+            <Navbar className="is-fixed-top">
+                <NavbarBrand>
+                    <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+                </NavbarBrand>
+                <NavbarMenu isActive={this.state.isActive}>
+                    <NavbarItem id="nav__profile" onClick={this.onClickNav}>Home</NavbarItem>
+                    <NavbarItem id="nav__search" onClick={this.onClickNav}>Projects</NavbarItem>
+                    <NavbarItem id="nav__suggest" onClick={this.onClickNav}>About Me</NavbarItem>
+                    <NavbarItem id="nav__logout" onClick={this.onClickNav}>Contact</NavbarItem>
+                </NavbarMenu>
+            </Navbar>
+        )
+    }
+}
+
+export default Nav
