4b51721289ffa1af7cc031c44924e197b367700a
Riley Mathews <rileymathews80@gmail.com>
2018-07-09 14:47:59 -0500

    added basic project component

---
+++ src/projects/Project.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Project extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <p>{this.props.name}</p>
+            </div>
+        )
+    }
+}
+
+export default Project
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Title } from 'bloomer/lib/elements/Title';
+import { Context } from '../Provider';
+import Project from './Project';
 
 
 class Projects extends Component {
@@ -9,6 +11,21 @@ class Projects extends Component {
         return (
             <div>
                 <Title isSize={2}>Projects</Title>
+                <Context.Consumer>
+                    {context => (
+                        <div>
+                            {context.state.projects.map(project => (
+                                <Project 
+                                    name={project.name}
+                                    description={project.description}
+                                    github_url={project.github_url}
+                                    live_url={project.live_url}
+                                />
+                            ))}
+                        </div>
+                    )
+                    }
+                </Context.Consumer>
             </div>
         )
     }
