23e7cd096aaa3e75fccb9b890913a8523155e441
Riley Mathews <rileymathews80@gmail.com>
2018-07-11 15:00:26 -0500

    made cards expand on hover

--- src/technologies/Technology.js
+++ src/technologies/Technology.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react'
 import './Technology.css'
 import { Column } from '../../node_modules/bloomer/lib/grid/Column';
 import { Card } from '../../node_modules/bloomer/lib/components/Card/Card';
-import { CardImage } from '../../node_modules/bloomer/lib/components/Card/CardImage';
-import { Image } from '../../node_modules/bloomer/lib/elements/Image';
 
 
 class Technology extends Component {
 
+    state = {
+        descriptionIsVisible: false
+    }
+
+    onCardHover = () => {
+        this.setState({ descriptionIsVisible: true })
+    }
+
+    onCardLeave = () => {
+        this.setState({ descriptionIsVisible: false })
+    }
+    
+    cardView = () => {
+        if (this.state.descriptionIsVisible) {
+            return <p>{this.props.technology.description}</p>
+        } else {
+            return (<img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />)
+        }
+    }
+
 
     render() {
         return (
-            <Column isSize={3}>
-                <Card>
-                    <CardImage>
-                        <Image isRatio='square' className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />
-                    </CardImage>
+            <Column isSize={2}>
+                <Card className={this.state.descriptionIsVisible ? "focused" : null}  onMouseEnter={this.onCardHover} onMouseLeave={this.onCardLeave}>
+                    {this.cardView()}
                 </Card>
             </Column>
         )
