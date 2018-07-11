7138c3a6286fe7d2f4b33fb9ba49d0b80279a709
Riley Mathews <rileymathews80@gmail.com>
2018-07-11 09:03:10 -0500

    added tech cards

--- src/technologies/Technology.js
+++ src/technologies/Technology.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './Technology.css'
 import { Column } from '../../node_modules/bloomer/lib/grid/Column';
+import { Card } from '../../node_modules/bloomer/lib/components/Card/Card';
+import { CardImage } from '../../node_modules/bloomer/lib/components/Card/CardImage';
+import { Image } from '../../node_modules/bloomer/lib/elements/Image';
 
 
 class Technology extends Component {
@@ -9,7 +12,11 @@ class Technology extends Component {
     render() {
         return (
             <Column isSize={3}>
-                <img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />
+                <Card>
+                    <CardImage>
+                        <Image isRatio='square' className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />
+                    </CardImage>
+                </Card>
             </Column>
         )
     }
