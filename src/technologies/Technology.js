9ce85620ed5440cc0fdb0fb01e8b68520dc41afa
Riley Mathews <rileymathews80@gmail.com>
2018-07-11 17:02:40 -0500

    made tech cards grow on over

--- src/technologies/Technology.js
+++ src/technologies/Technology.js
@@ -17,21 +17,24 @@ class Technology extends Component {
     onCardLeave = () => {
         this.setState({ descriptionIsVisible: false })
     }
-    
-    cardView = () => {
-        if (this.state.descriptionIsVisible) {
-            return <p>{this.props.technology.description}</p>
-        } else {
-            return (<img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />)
-        }
-    }
+
+    // cardView = () => {
+    //     if (this.state.descriptionIsVisible) {
+    //         return <p>{this.props.technology.description}</p>
+    //     } else {
+    //         return (<img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />)
+    //     }
+    // }
 
 
     render() {
         return (
-            <Column isSize={2}>
-                <Card className={this.state.descriptionIsVisible ? "focused" : null}  onMouseEnter={this.onCardHover} onMouseLeave={this.onCardLeave}>
-                    {this.cardView()}
+            <Column isSize={2} className={this.state.descriptionIsVisible ? "column_focused" : null}>
+                <Card className={this.state.descriptionIsVisible ? "tech_card card_focused" : "tech_card"} onMouseEnter={this.onCardHover} onMouseLeave={this.onCardLeave}>
+                    <img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />
+                    <Card className={this.state.descriptionIsVisible ? "tech_modal_shown" : "tech_modal_hidden"}>
+                        <p>{this.props.technology.description}</p>
+                    </Card>
                 </Card>
             </Column>
         )
