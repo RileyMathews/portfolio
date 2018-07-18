d82ca400336458f75a2d5c41d9cdbcad83129280
Riley Mathews <rileymathews80@gmail.com>
2018-07-18 10:51:11 -0500

    added 404 page

---
+++ src/Error404.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Title } from '../node_modules/bloomer/lib/elements/Title';
+
+
+class Error404 extends Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <Title>Error 404</Title>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Error404
--- src/router/ApplicationViews.js
+++ src/router/ApplicationViews.js
@@ -1,20 +1,22 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import React, { Component } from "react"
 import AboutView from '../about/AboutView';
 import HomePage from '../home/HomePage';
 import Projects from '../projects/Projects';
 import ContactView from '../contact/ContactView';
+import Error404 from '../Error404';
 
 
 export default class ApplicationViews extends Component {
     render() {
         return (
-            <div>
+            <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/projects" component={Projects} />
                 <Route path="/about" component={AboutView} />
                 <Route path="/contact" component={ContactView} />
-            </div>
+                <Route component={Error404} />
+            </Switch>
         )
     }
 }
