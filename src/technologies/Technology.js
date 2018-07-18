c3b0b21b78ee81f46e996469ba1456cf8621ff98
Riley Mathews <rileymathews80@gmail.com>
2018-07-17 22:37:44 -0500

    made tech drop downs mobile friendly

--- src/technologies/Technology.js
+++ src/technologies/Technology.js
@@ -8,26 +8,47 @@ import $ from 'jquery';
 class Technology extends Component {
 
     state = {
-        descriptionIsVisible: false
+        descriptionIsVisible: false,
+        dropDownActive: false,
+        userIsScrolling: false,
     }
 
-    onCardHover = () => {
+    showDropDown = (evt) => {
+        console.log("mouse entered")
         $(`#dropdown_tech_${this.props.technology.id}`).slideDown()
+        this.setState({ dropDownActive: true})
     }
 
-    onCardLeave = () => {
+    hideDropDown = (evt) => {
+        console.log("mouse left")
         $(`#dropdown_tech_${this.props.technology.id}`).slideUp()
+        this.setState({ dropDownActive: false})
     }
 
-    toggleDropDown = () => {
-        $(`#dropdown_tech_${this.props.technology.id}`).slideToggle()
+    toggleDropDown = (evt) => {
+        console.log("mouse entered")
+        if (!this.state.userIsScrolling) {
+            if (this.state.dropDownActive) {
+                $(`#dropdown_tech_${this.props.technology.id}`).slideUp()
+                this.setState({ dropDownActive: !this.state.dropDownActive })
+            } else {
+                $(`#dropdown_tech_${this.props.technology.id}`).slideDown()
+                this.setState({ dropDownActive: !this.state.dropDownActive })
+            }
+        } else {
+            this.setState({ userIsScrolling: false })
+        }
+    }
+
+    setScrolling = () => {
+        this.setState({ userIsScrolling: true })
     }
 
 
     render() {
         return (
             <Column isSize={3} className="dropdown">
-                <Card className="dropdown-hover" onMouseEnter={this.onCardHover} onMouseLeave={this.onCardLeave} onClick={this.onCardLeave}>
+                <Card className="dropdown-hover" onMouseEnter={this.showDropDown} onMouseLeave={this.hideDropDown} onTouchEnd={this.toggleDropDown} onTouchMove={this.setScrolling}>
                     <img className="tech_logo" src={this.props.technology.img_url} alt={`${this.props.technology.name} logo`} />
                     <Card className="dropdown-content" id={`dropdown_tech_${this.props.technology.id}`}>
                         <p className="tech_description">{this.props.technology.description}</p>
